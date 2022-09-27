d4669a6f8620348d1760c271fde11c4782398f87
Bill T <79499317+Ktasa@users.noreply.github.com>
2022-09-27 15:30:28 -0700

    added function headers

---
+++ ComplexPlane.cpp
@@ -0,0 +1,24 @@
+#include <complex>      // std::complex, std::abs
+#include <SFML/Graphics.hpp>
+#include "ComplexPlane.h"
+using namespace std;
+using namespace sf;
+
+    ComplexPlane::ComplexPlane(double aspectRatio)
+    {}
+    void ComplexPlane::zoomIn()
+    {}
+    void ComplexPlane::zoomOut()
+    {}
+    void ComplexPlane::setCenter(Vector2f coord)
+    {}
+    void ComplexPlane::setMouseLocation(Vector2f coord)
+    {}
+    void ComplexPlane::loadText(Text& text)
+    {}
+    View ComplexPlane::getView()
+    {}
+    size_t ComplexPlane::countIterations(Vector2f coord)
+    {}
+    void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+    {}

db91d9c12157faf7279e79f6c406cb50b30de736
Bill Tasabia <billtasabia@gmail.com>
2022-09-27 15:14:02 -0700

    updated

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -16,9 +16,9 @@ public:
     void zoomIn();
     void zoomOut();
     void setCenter(Vector2f coord);
-    View getView();
     void setMouseLocation(Vector2f coord);
     void loadText(Text& text);
+    View getView();
     size_t countIterations(Vector2f coord);
     void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
 
